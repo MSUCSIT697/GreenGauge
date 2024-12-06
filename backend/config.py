cc8867409cd105d14be66b461f38b8186a66ee25
Tejaswi Manukonda <manukondat1@montclair.edu>
2024-12-05 20:52:37 -0500

    Initial Commit

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+class Config:
+    MYSQL_HOST = 'database-1.c122k88askq1.us-east-1.rds.amazonaws.com'
+    MYSQL_USER = 'admin'
+    MYSQL_PASSWORD = 'x5*lpistL6et8er*_'
+    MYSQL_DB = 'emissions_db'
---
+++ backend/run.py
@@ -0,0 +1,7 @@
+# from app import create_app
+
+# # Initialize the Flask app and run it
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
