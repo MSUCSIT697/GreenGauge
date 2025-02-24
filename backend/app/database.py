4c99b62d2f6283e9d5af4cb9ed9752b12eaf76ee
Tejaswi Manukonda <manukondat1@montclair.edu>
2025-02-24 18:20:07 -0500

    Db changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,9 +3,14 @@ from flask import current_app
 
 # Function to get a database connection
 def get_db_connection():
-    return mysql.connector.connect(
-        host=current_app.config['MYSQL_HOST'],
-        user=current_app.config['MYSQL_USER'],
-        password=current_app.config['MYSQL_PASSWORD'],
-        database=current_app.config['MYSQL_DB']
-    )
+    try:
+        return mysql.connector.connect(
+            host=current_app.config["MYSQL_HOST"],
+            user=current_app.config["MYSQL_USER"],
+            password=current_app.config["MYSQL_PASSWORD"], 
+            database=current_app.config["MYSQL_EMISSIONS_DB"],
+            port=int(current_app.config["MYSQL_PORT"])
+        )
+    except mysql.connector.Error as err:
+        print(f"Database Connection Error: {err}")
+        return None
--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="/var/www/backend/.env")
 
 class Config:
-    MYSQL_HOST = os.getenv("DATABASE_HOST")
-    MYSQL_USER = 'admin'
-    MYSQL_PASSWORD = os.getenv("DATABASE_PWD")
-    MYSQL_DB = 'emissions_db'
+    MYSQL_HOST = os.getenv("RDS_HOST")
+    MYSQL_USER = os.getenv("RDS_USER")
+    MYSQL_PASSWORD = os.getenv("RDS_PASSWORD")
+    MYSQL_EMISSIONS_DB = 'emissions_db'
+    MYSQL_PORT = os.getenv("RDS_PORT")
